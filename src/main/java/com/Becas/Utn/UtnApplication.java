c7e0641fbbe794175dd37e3122f6cf6b09a4e813
Lautaro Fernandez Esposito <lfernandezesposito@gmail.com>
2026-01-19 21:45:54 -0300

    Proyecto Inicial

---
+++ src/main/java/com/Becas/Utn/UtnApplication.java
@@ -0,0 +1,14 @@
+package com.Becas.Utn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnApplication.class, args);
+		System.out.println("Funciona");
+	}
+
+}
---
+++ src/test/java/com/Becas/Utn/UtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Becas.Utn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
