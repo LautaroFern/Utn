f19f630f5cbdada67bf768b8f22dae402b673640
Lautaro Fernandez Esposito <lfernandezesposito@gmail.com>
2026-02-16 22:10:48 -0300

    Comenzando a modelar el Back-End

---
+++ backend/src/main/java/com/Becas/Utn/Model/Usuario.java
@@ -0,0 +1,103 @@
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+// Creacion de atributos de la clase Usuario
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    @Column (nullable = false)
+    private String  password;
+    private Boolean estado;
+
+    public Usuario(){
+        this.nombre= "";
+        this.apellido= "";
+        this.email= "";
+        this.password= "";
+        this.estado= true;
+    }
+
+    public Usuario(String nombre, String apellido, String email, String password){
+        this.nombre= nombre;
+        this.apellido= apellido;
+        this.email= email;
+        this.password= password;
+        this.estado= true;
+    }
+
+    // Getters Y Setters de la clase Usuario
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+        public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    // Metodos Equals , HashCode y ToString
+    @Override
+    public boolean equals (Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id != null && id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString(){
+        return "Usuario: " + 
+        "Id: " + this.id +
+        "Nombre: " + this.nombre + 
+        "Apellido: " + this.apellido +
+        "Email: " + this.email;
+    }
+}
+
